72bdd146dbb2c575d106389e2b0a2e13dffd65f7
charouzic <viki.macek@gmail.com>
2020-09-28 18:38:22 +0200

    Update Program.cs

--- Macek_mandatoryAssignment2_private/Program.cs
+++ Macek_mandatoryAssignment2_private/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Macek_mandatoryAssignment2
 {
@@ -7,30 +7,17 @@ namespace Macek_mandatoryAssignment2
         public static void Main(string[] args)
         {
 
-            int xprice = 120000;
-            int yprice = 75000;
-            int stockX = 100;
-            int stockY = 12;
-
+            
             // creating object and filling it with variables
             CarStock carStock = new CarStock();
 
-            Console.Write("Please provide username to access the BrandX system: ");
-            string userName = Console.ReadLine();
-            Console.Write("Please provide password: ");
-            string pwd = Console.ReadLine();
+            carStock.XPrice = 120000;
+            carStock.YPrice = 75000;
+            carStock.TotalInStockX = 10;
+            carStock.TotalInStockY = 19;
 
             // calling function that checks the login details
-            carStock.Login(userName, pwd, xprice, yprice, stockX, stockY);
+            carStock.Login();
         }
     }
 }
-
-/* I had issue with writing the code in a way that I can give the user option to
-   have multiple attempts to log into the program. Also what I found a little bit
-   tricky was to assign the values to stock size and price in the main function
-   since I wanted to make the functions hidden as private one otherwise the login
-   functionality wouldn't make any sense.
-   Also what I found difficult is to write the code, methods and classes in a way
-   that they are reusable as much as possible. 
-*/
